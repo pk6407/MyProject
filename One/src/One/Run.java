431a83165b07f92a7b9c0167e6e37b8e9e01f678
pk6407 <73266533+pk6407@users.noreply.github.com>
2020-10-22 19:33:25 +0900

    Add files via upload

---
+++ One/src/One/Run.java
@@ -0,0 +1,11 @@
+package One;
+import One.CoffeeShopMenu;
+import One.CoffeeShop;
+public class Run {
+
+	public static void main(String[] args) {
+		CoffeeShopMenu a = new CoffeeShopMenu();
+		a.Menu();
+	}
+
+}
---
+++ One/src/One/login.java
@@ -0,0 +1,6 @@
+package One;
+
+public class login {
+	private String id = ("hanna"); 
+	private int pwd = 1234;
+}
